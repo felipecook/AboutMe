e61859a2c7759a07682b853dab981605f28362e7
Felipe Cook <felipepcook@gmail.com>
2022-07-23 08:29:44 +0800

    added code (using bindings instead of R.findviewbyid method) to link button to edit text and text view

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.android.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.android.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.bind(findViewById(R.id.root))
+
+        binding.doneButton.setOnClickListener{
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+
+        val editText = binding.nicknameEdit
+        val nicknameTextView = binding.nicknameText
+
+        nicknameTextView.text = editText.text
+
+        // this hides the edit text
+        editText.visibility = View.GONE
+
+        // this hides the view aka the button
+        view.visibility = View.GONE
+
+        nicknameTextView.visibility = View.VISIBLE
+
+        //hide the keyboard code
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
 }
